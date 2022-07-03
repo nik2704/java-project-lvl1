8e309a238c08732a2fc72108cbf693d7841b79ef
Nikolay Zenchenko <zenchenko.nikolay@microfocus.com>
2022-07-03 22:46:13 +0300

    03_07_2022 Task3

---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static void greeting() {
+        System.out.println("Welcome to the Brain Games!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String userName = scanner.nextLine();
+        System.out.println("Hello, " + userName + "!");
+    }
+}
